c7b06bafa25510359a27c7be7178250581fd9780
open17 <you@example.com>
2023-12-23 15:21:12 +0800

    feat: 数论内容更新

--- math/number-theory/exgcd.py
+++ math/number-theory/exgcd.py
@@ -1,15 +1,14 @@
 """
 扩展欧几里得用于求出ax+by=gcd(a,b)的一组特解
 通解即为 x'=x+k*b/(gcd(a,b)) y'=y-k*a/(gcd(a,b))
+
+同时显然知道扩展欧几里得后,裴蜀定理的正确性自然一目了然
+
+>>> exgcd(3,10)
+    (1, -3, 1)
 """
 
 def exgcd(a, b):
-    """
-    return gcd(a,b) and a pair (x,y) of func ax+by=gcd(a,b) 
-
-    >>> exgcd(3,10)
-    (1, -3, 1)
-    """
     if b==0: return a,1,0
     d, x, y = exgcd(b, a % b)
     return d, y, x - (a // b) * y
