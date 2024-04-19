2399be04715086968d92e8444253243a60cae91d
open17 <opensci@163.com>
2024-04-20 02:08:00 +0800

    feat: 更新自动打卡

--- auto/auto_push_cf.py
+++ auto/auto_push_cf.py
@@ -2,11 +2,11 @@ import os
 import re
 import shutil
 
-folder_path = './code/CodeForce'
+folder_path = './code/Codeforces'
 target_folder='./Daily_CF_Problems'
 target_path='./Daily_CF_Problems/daily_problems'  
+CODE_LIST=['cpp','py']
 NAME="Open17"
-pattern = r'\[(.*?)\]'
 
 def list_files(directory):
     for root, dirs, files in os.walk(directory):
@@ -31,15 +31,17 @@ print("自动化打卡,启动!")
 
 print("读取文件列表...")
 for file_path in list_files(folder_path):
-    matches = re.findall(pattern, file_path)
-    if matches:
-        date_list=matches[-1].split('-')
-        if len(date_list)!=3:continue
-        dir=date_list[0]+"/"+date_list[1]+"/"+date_list[1]+date_list[2]
-        destination_path=os.path.join(target_path,dir)+'/personal_submission/'+'cf'+re.sub(pattern, '_'+NAME, file_path.split('/')[-1])
-        print("自动化打卡生成:   ",destination_path)
-        copy_file(file_path, destination_path)
-        os.system('cd ' + target_folder + ' && git add . && git commit -m "' + ''.join(date_list) + ' ' + NAME + '\'s submission"')
-    
-
+    tmp=file_path.strip(folder_path).split('.')
+    if len(tmp)!=2:continue
+    f_name,f_type=tmp
+    f_name=f_name.strip('/').strip('\\')
+    if f_type not in CODE_LIST:continue
+    f_name_list=f_name.split('-')
+    if len(f_name_list)!=4:continue
+    date_list=f_name_list[1:]
+    dir=date_list[0]+"/"+date_list[1]+"/"+date_list[1]+date_list[2]
+    destination_path=os.path.join(target_path,dir)+'/personal_submission/'+'cf'+f_name_list[0]+'_'+NAME+'.'+f_type
+    print("生成打卡目标:   ",destination_path)
+    copy_file(file_path, destination_path)
+    os.system('cd ' + target_folder + ' && git add . && git commit -m "' + ''.join(date_list) + ' ' + NAME + '\'s submission"')
 print("自动化打卡完成!")
---
+++ code/Codeforces/1878D-2024-04-19.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define ios ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+#define read() ({ int x = 0, f = 1; char ch = getchar(); while (ch < '0' || ch > '9') { if (ch == '-') f = -1; ch = getchar(); } while (ch >= '0' && ch <= '9') { x = x * 10 + ch - '0'; ch = getchar(); } x * f; })
+  
+const int N = 2e5 + 10;
+
+signed main()
+{
+    // ios;
+    int a = read();
+    cout<<a;
+    return 0;
+}
